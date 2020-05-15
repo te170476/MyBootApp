bee9c96d17471f3ef7a4bf5131d1d4ae1a829e2d
tcc170476 <170476@jc-21.jp>
2020-05-15 11:02:52 +0900

    :tada: first commit.

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp4;
+package jp.te4a.spring.boot.myapp5;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
