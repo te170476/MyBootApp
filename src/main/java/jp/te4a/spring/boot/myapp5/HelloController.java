d293fb066cfd23a9cf769f7a6ed0d709f34cc500
tcc170476 <170476@jc-21.jp>
2020-05-19 11:11:25 +0900

    write to load index.jsp in HelloController.java

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -1,12 +1,13 @@
 package jp.te4a.spring.boot.myapp5;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	@RequestMapping("/")
-	public String index() {
-		return "this is Spring Boot sample.";
+	public String index(Model model) {
+		return "index";
 	}
 }
