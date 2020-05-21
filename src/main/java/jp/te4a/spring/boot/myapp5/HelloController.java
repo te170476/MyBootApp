3e358089b0b8ea929ac1f9db815fb496d9adde86
tcc170476 <170476@jc-21.jp>
2020-05-21 10:19:07 +0900

    add Attribute in HelloController#index(Model)

--- src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp5/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 	@RequestMapping("/")
 	public String index(Model model) {
+		model.addAttribute("msg", "this is a setting message");
 		return "index";
 	}
 }
