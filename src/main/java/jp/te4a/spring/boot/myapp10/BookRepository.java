ee87546a2f11e1d0bb938d0bb844dbb405268498
te170476 <170476@jc-21.jp>
2020-06-09 15:07:42 +0900

    :tada: first commit.

---
+++ src/main/java/jp/te4a/spring/boot/myapp10/BookRepository.java
@@ -0,0 +1,7 @@
+package jp.te4a.spring.boot.myapp10;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<BookBean, Integer>{
+
+}
